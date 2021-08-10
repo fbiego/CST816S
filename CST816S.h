8e2eee1b7a3b12465802f465f866ae758a815588
Felix Biego <fbiego.fb@gmail.com>
2021-08-10 20:28:08 +0300

    Update CST816S.h

--- CST816S.h
+++ CST816S.h
@@ -27,45 +27,45 @@
 
 #include <Arduino.h>
 
-#define CST816S_NONE			0x00
-#define CST816S_SWIPE_DOWN		0x01
-#define CST816S_SWIPE_UP		0x02
-#define CST816S_SWIPE_LEFT		0x03
-#define CST816S_SWIPE_RIGHT		0x04
-#define CST816S_SINGLE_CLICK	0x05
-#define CST816S_DOUBLE_CLICK	0x0B
-#define CST816S_LONG_PRESS		0x0C
+#define CST816S_NONE 			0x00
+#define CST816S_SWIPE_DOWN 		0x01
+#define CST816S_SWIPE_UP 		0x02
+#define CST816S_SWIPE_LEFT 		0x03
+#define CST816S_SWIPE_RIGHT 	0x04
+#define CST816S_SINGLE_CLICK 	0x05
+#define CST816S_DOUBLE_CLICK 	0x0B
+#define CST816S_LONG_PRESS 		0x0C
 
-#define CST816S_ADDRESS			0x15
+#define CST816S_ADDRESS 		0x15
 
-struct touch_data_struct{
-	byte gesture;
+struct touch_data_struct {
+  byte gesture;
   byte points;
   byte event;
   int x;
   int y;
-}touch_data ;
 
+  uint8_t version;
+  uint8_t versionInfo[3];
+}
+touch_data;
 
 class CST816S {
-	
-	public:
-		CST816S(int sda, int scl, int rst, int irq);
-		void begin();
-		void end();
-		void sleep(bool state);
-		uint8_t version;
-		uint8_t versionInfo[3];
-	private:
-		int _sda;
-		int _scl;
-		int _rst;
-		int _irq;
-		bool _touch_enable;
-		
-		uint8_t i2c_read(uint16_t addr, uint8_t reg_addr, uint8_t *reg_data, uint32_t length);
-		uint8_t i2c_write(uint8_t addr, uint8_t reg_addr, const uint8_t *reg_data, uint32_t length);
-};
 
+  public:
+    CST816S(int sda, int scl, int rst, int irq);
+	void begin();
+	void end();
+	void sleep(bool state);
+  private:
+    int _sda;
+	int _scl;
+	int _rst;
+	int _irq;
+	bool _touch_enable;
+
+	uint8_t i2c_read(uint16_t addr, uint8_t reg_addr, uint8_t * reg_data, uint32_t length);
+	uint8_t i2c_write(uint8_t addr, uint8_t reg_addr, const uint8_t * reg_data, uint32_t length);
+};
 
 #endif
